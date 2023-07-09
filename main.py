3e65c8548bca1ca32e39bd65cef1d05ea0124e1b
Thomas Holmquist <thomas@vorget.com>
2023-07-09 14:04:39 +0200

    Further implementation

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from typing import Optional
-from fastapi import FastAPI
 from pathlib import Path, PurePath
 
+from fastapi import FastAPI
+from starlette.requests import Request
+
 app = FastAPI()
+
+# HOOK_DIR /run/fasthook is created by systemd via the RuntimeDirectory option. It's deleted when the service stops.
+HOOK_DIR = PurePath("/run/fasthook")
+
+DECODE_FORMAT = "latin-1"
 
 
 @app.get("/")
@@ -11,12 +17,29 @@ async def read_root():
 
 
 @app.post("/github/{target}/{hook}")
-async def read_github(target: str, hook: str, github_payload: Optional[str] = None):
+async def read_github(target: str, hook: str):
     """
     Github webhook handler
     """
-    hookpath = Path(PurePath("/tmp/github").joinpath(target, hook))
-    if hookpath.is_relative_to(PurePath("/tmp/github")):
-        hookpath.parent.mkdir(parents=True, exist_ok=True)
+    hookpath = Path(HOOK_DIR.joinpath(target, hook))
+    if hookpath.is_relative_to(HOOK_DIR):
+        hookpath.parent.mkdir(parents=False, exist_ok=True)
         hookpath.touch()
-    return {"github_payload": github_payload}
+        return {"github_payload": "ok"}
+    else:
+        return {"github_payload": "error"}
+
+
+@app.middleware("http")
+async def case_sens_middleware(request: Request, call_next):
+    """
+    Make url lowercase
+    """
+    raw_query_str = request.scope["query_string"].decode(DECODE_FORMAT).lower()
+    request.scope["query_string"] = raw_query_str.encode(DECODE_FORMAT)
+
+    path = request.scope["path"].lower()
+    request.scope["path"] = path
+
+    response = await call_next(request)
+    return response
